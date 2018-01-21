de69a9ce98f55e87a299f9c367970376fe5415aa
Allenas <gao.bolun@Gmail.com>
2018-01-20 23:03:34 -0700

    create a window

--- main.cpp
+++ main.cpp
@@ -1,50 +1,74 @@
 ﻿#include <GL/glew.h>
 #include <GL/freeglut.h>
+
 #include <iostream>
+using namespace std;
 
-void render(void);
+void myRender(void);
 
-void keyboard(unsigned char c, int x, int y);
+void myKeyboard(unsigned char key, int x, int y);
 
-void mouse(int button, int state, int x, int y);
 
-int main(int argc, char** argv) {
+int main(int argc, char *argv[]) {
+
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(640, 480);
-	glutCreateWindow("Simple GLUT Application");
+	glewInit();
 
-	glutDisplayFunc(render);
-	glutKeyboardFunc(keyboard);
-	glutMouseFunc(mouse);
+	cout << "Bolun" << endl;
+	cout << glGetString(GL_VERSION) << endl;
+
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 720);
+	glutInitWindowPosition(100, 100);
+
+
+	glutCreateWindow("OpenGL Window");
+
+	glutDisplayFunc(myRender);
+	glutKeyboardFunc(myKeyboard);
+
+	if (glewInit() != GLEW_OK) {
+		cout << "ERROR - glew not included" << endl;
+	}
 
 	glutMainLoop();
 }
 
-void keyboard(unsigned char c, int x, int y) {
-	if (c == 27) {
+void myKeyboard(unsigned char key, int x, int y) {
+	if (key == 27) {
 		exit(0);
 	}
 }
 
-void mouse(int button, int state, int x, int y) {
-	if (button == GLUT_RIGHT_BUTTON) {
-		exit(0);
-	}
-}
+void myRender(void) {
 
-void render(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glBegin(GL_TRIANGLES);
-	glColor3f(1, 0, 0);
-	glVertex2f(-0.5, -0.5);
-	glColor3f(0, 1, 0);
-	glVertex2f(0.5, -0.5);
-	glColor3f(0, 0, 1);
-	glVertex2f(0.0, 0.5);
-	glEnd();
+	float vertex[15] = {
+		1, 0, 0,
+		-0.5, -0.5,
+		0, 1, 0,
+		0.5, -0.5,
+		0, 0, 1,
+		0.0, 0.5
+	};
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 15 * sizeof(float), vertex, GL_STATIC_DRAW);
+
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glEnableVertexAttribArray(0);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)12);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//glDisableVertexAttribArray(0);
 
 	glutSwapBuffers();
 }
