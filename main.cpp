53477df0b34b2879183dc315be1aae0a07c5481a
Allenas <gao.bolun@Gmail.com>
2018-01-29 15:40:32 -0700

    working on obj import

--- main.cpp
+++ main.cpp
@@ -5,21 +5,49 @@
 	*/
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-
+#include "cyMatrix.h"
 #include <iostream>
 using namespace std;
+
+const char* vertexShader = "shader.vs";
+const char* fragmentShader = "shader.fs";
 
 float colorR;
 float colorG;
 float colorB;
 
+GLuint VBO;
+
+/*
+	set background color
+*/
 void myRender(void) {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glClearColor(colorR, colorG, colorB, 0.0f);
 
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(0);
+
 	glutSwapBuffers();
+}
+
+static void vertexBuffer() {
+	float vertices[9] = {
+		0.5f, 0.5f, 0.5f,
+		0.5f, 0.5f, 0.5f,
+		0.5f, 0.5f, 0.5f
+	};
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 }
 
 void myKeyboard(unsigned char key, int x, int y) {
@@ -28,21 +56,28 @@ void myKeyboard(unsigned char key, int x, int y) {
 	}
 }
 
-void timer(int value) {
+/*
+	animate background color by using glutPostRedisplay
+*/
+void idle(int value) {
 
+	// random num between 0 and 1
 	colorR = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 	colorG = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 	colorB = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 
 	myRender();
 	glutPostRedisplay();
-	glutTimerFunc(1000, timer, 3);
+	glutTimerFunc(33, idle, 1);
+}
+
+static void initialShader(GLuint shaderProgram, const char* shaderName, GLenum shaderType) {
+
 }
 
 int main(int argc, char *argv[]) {
 
 	glutInit(&argc, argv);
-	glewInit();
 
 	cout << "Bolun" << endl;
 
@@ -50,16 +85,21 @@ int main(int argc, char *argv[]) {
 	glutInitWindowSize(1024, 720);
 	glutInitWindowPosition(100, 100);
 
-
-	glutCreateWindow("OpenGL Window");
+	glutCreateWindow("OpenGL Window - Bolun Gao");
 
 	glutDisplayFunc(myRender);
-	glutTimerFunc(1000, timer, 3);
-	glutKeyboardFunc(myKeyboard);
 
 	if (glewInit() != GLEW_OK) {
 		cout << "ERROR - glew not included" << endl;
-	}
+	} 
+
+	glutTimerFunc(33, idle, 1);    //loop animation
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	glutKeyboardFunc(myKeyboard);
+
+	vertexBuffer();
 
 	glutMainLoop();
 }
